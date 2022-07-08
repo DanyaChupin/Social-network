6a7d634ff73b2b4b26b0b7c375e220a13d8b2554
Danya <Chupa.chupa.14@mail.ru>
2022-07-08 12:54:28 +0300

    Modules JSX

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.itl.cat/pngfile/big/15-154475_2k-mac-os-sierra.jpg" alt=""/>
+            </div>
+            <div>
+                ava + discr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
